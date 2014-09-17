7e705108861d7a745d9135b99bf8d9c5c06bb3a3
Heark <hearkofsorcerey@gmail.com>
2014-09-17 10:19:27 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// (C)opyright Heark Gaming Co.
+// Special Thanks Coyotte508
+// Created 9/17/2014
+
+step : function() {
+    sys.changeAnnouncement("<head>
+<script>
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h+":"+m+":"+s;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+</script>
+</head>
+
+<body onload="startTime()">
+
+<div id="txt"></div>")
+}
+ 
