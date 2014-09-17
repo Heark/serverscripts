a7b3bff17855b43cfb087d2357430eaf470ca1b7
Heark <hearkofsorcerey@gmail.com>
2014-09-17 10:21:29 -0400

    Create time.js

---
+++ time.js
@@ -0,0 +1,4 @@
+var doc = this;
+var type = lang
+doc.type = html("<table style="background-color:#FF0000FF"><tr><td width="100%">
+<p><center><font size=48 color=black>❖</font><font size=12 color=white> Pokemon </font><font size=24 color=black>❖</font></center><br /><br /><center><b>The current time is: %%1</b></center></p></td></tr></table>")
